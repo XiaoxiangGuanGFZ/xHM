0a834e0271fa0389672426f4049b43724a1f83f1
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-07-25 16:15:35 +0200

    initialize the radiation calculation functions

--- scr/SnowEnergy.c
+++ scr/SnowEnergy.c
@@ -110,20 +110,3 @@ double FLUX_advect(
 }
 
 
-/**
- * to simulate the snow accumulation and melting process in a physical-based manner,
- * the information about (shortwave and longwave) radiation flux
- * is required as input for energy balance iteration. The radiation flux is either obtained from 
- * instrumental observation or empirically estimated from other meteorological variables, like 
- * air temperature, relative humidity, atmospheric pressure, sunshine duration and wind speed. 
-*/
-
-
-/***
- * empirical formula for radiation computation:
- * - extraterrestial (shortwave) radiation from sun
- * - longwave radiation 
- * - 
-*/
-
-
