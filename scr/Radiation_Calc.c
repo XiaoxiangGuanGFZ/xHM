ff198ad5a0c1ac6c869cff2933982447f5e3242c
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-11-27 12:55:53 +0100

    enrich info in Radiation_calc #6

--- scr/Radiation_Calc.c
+++ scr/Radiation_Calc.c
@@ -1,5 +1,16 @@
-/******
- * to simulate the snow accumulation and melting process in a physical-based manner,
+/*
+ * SUMMARY:      Radiation_Calc.c
+ * USAGE:        Calculate short and long wave radiations
+ * AUTHOR:       Xiaoxiang Guan
+ * ORG:          Section Hydrology, GFZ
+ * E-MAIL:       guan@gfz-potsdam.de
+ * ORIG-DATE:    Aug-2023
+ * DESCRIPTION:  Calculate radiations
+ * DESCRIP-END.
+ * FUNCTIONS:    
+ * 
+ * COMMENTS:
+ * In order to simulate the snow accumulation and melting process in a physical-based manner,
  * the information about (shortwave and longwave) radiation flux
  * is required as input for energy balance iteration. The radiation flux is either obtained from 
  * instrumental observation or empirically estimated from other meteorological variables, like 
@@ -23,7 +34,7 @@
  * - relative humidity, %
  * - sunshine duration, hours
  * - LAI
- * - forest albedo
+ * - forest (canopy) albedo
  * - forest (canopy) fraction, between 0 and 1
  * - snow albedo (assumed as 0.5)
  * 
@@ -53,31 +64,29 @@ int NOD(
     return NOD;
 }
 
+/************ shortwave radiation *************/
 
 double Radiation_downward_short(
     int year,
     int month,
     int day,
-    double lat,  // the latitute of the location
-    double n,   // sunshine duration in a day, hours
-    // two parameters, 0.25 and 0.5 by default
+    double lat,  /* the latitute of the location */ 
+    double n,    /* sunshine duration in a day, hours */ 
     double as,
-    double bs    
+    double bs    /* as and bas: two empirical coefficients, 0.25 and 0.5 by default */
 ){
-    /*
-    received shortwave radiation, considering the effect of cloudiness
+    /*****
+     *  calculate the received shortwave radiation [(MJ⋅m-2⋅d-1)], 
+     *  considering the effect of cloudiness, at a daily step
+     * 
     */
     double tau_cloud;  // effect of cloud cover on insolation
-
-    double R_et;  // extraterrestrial radiation
-    double R_cs;  // clear-sky radiation
-
-    int J; // number of day
+    double R_et;       // extraterrestrial radiation
+    double R_cs;       // clear-sky radiation
+    int J; // number of the day
     J = NOD(year, month, day);
 
-    //****计算地球外辐射，MJ/m2/d********
-    // extraterrestrial radiation (MJ⋅m-2 d-1)
-
+    // estimate the extraterrestrial radiation (MJ⋅m-2 d-1)
     double dr, del, w_s, Ra; // intermediate variable
 
     //the inverse relative distance between earth and sun
@@ -93,33 +102,34 @@ double Radiation_downward_short(
     R_et = 37.59 * dr * (w_s * sin(lat * PI / 180) * sin(del) + cos(lat * PI / 180) * cos(del) * sin(w_s));
     // 37.59: 24(60)/PI*Gsc; Gsc is the solar constant and equals to 0.082MJ⋅m(-2)⋅min(-1)
 
-    R_cs = (as + bs) * R_et;
+    R_cs = (as + bs) * R_et;  // clear-sky radiation, non-cloudy
     
-    tau_cloud = as + bs * n / (24/PI * w_s);
+    tau_cloud = as + bs * n / (24/PI * w_s);  // diminishing effect from cloudiness
     
-    // double tau_canopy; // effect of forest canopy on insolation
-    // double albedo_forest,  //  the albedo of the vegetation
-    // double LAI,
-    // double albedo_snow,    // the albedo of the snow under the canopy (assumed to be 0.5)
-    // tau_canopy = (1 - albedo_forest) * (1 - albedo_snow) * exp(-LAI);
-
-    return R_cs * tau_cloud; //* tau_canopy
+    return R_cs * tau_cloud; 
 
 }
 
 double Radiation_short_surface(
     double R_sky,  /*the received solar radiation, considering the effect of cloudiness, output from 
      from function Radiation_downward_short() */ 
-    double albedo_forest,  //  the albedo of the vegetation
-    double LAI,    // leaf area index, dimensionless
+    double albedo_forest,  // the albedo of the vegetation
+    double LAI,            // leaf area index, dimensionless
     double albedo_snow     // the albedo of the snow under the canopy (assumed to be 0.5)
 ){
-    /*
+    /*****
+     * calculate the received shortwave radiation 
+     *  on the understory ground (or snow) surface
+     * 
+    */
+
+    /* applicable scenarios: 
     received shortwave radiation on the SURFACE of
     - canopy: albedo_snow = 0.0
     - canopy snow: albedo_snow = 0.5
     - ground: albedo_snow = 0.0
     - ground snow: albedo_snow = 0.5
+
     when there is no canopy, albedo_forest = 0.0 and LAI = 0.0
     */
 
@@ -128,19 +138,21 @@ double Radiation_short_surface(
    return R_sky * tau; 
 }
 
+/************ longwave radiation *************/
+
 double Radiation_long_surface(
     double Tem_air,  // air temperature, Celsius degree
     double RHU,   // relative humidity, unit: %
     double FF  // the fractional forest cover, between 0.0 and 1.0
 ){
-    /*
-    received longwave radiation on the surface
-    reference: 
-        Abramowitz G., L. Pouyanné, and H. Ajami (2012). 
-        On the information content of surface meteorology for 
-        downward atmospheric long-wave radiation synthesis. 
-        Geographical Research Letters 39(5), 
-        doi: https://doi.org/10.1029/2011GL050726
+    /*****
+     * calculate the received longwave radiation on the surface
+     *      in an empirical manner.
+     * Abramowitz et al. (2012) found that 
+     *      under both clear and cloudy conditions, 
+     *      the received longwave radiation on the surface 
+     *      can be estimated with a simple empirical relation 
+     *      well without considering the clouds correcting:
     */
 
     double es;  // saturated vapor pressure, kPa;
@@ -168,19 +180,30 @@ double Radiation_downward_long(
     int year,
     int month,
     int day,
-    double lat,  // the latitute of the location
-    double Tem_air,  // air temperature, Celsius degree
-    double RHU,   // relative humidity, unit: %
-    double n,  // sunshine duration in a day, hours
-    double FF  // the fractional forest cover, between 0.0 and 1.0
+    double lat,      /* the latitute of the location */ 
+    double Tem_air,  /* air temperature, [Celsius degree] */ 
+    double RHU,      /* relative humidity, unit: % */ 
+    double n,        /* sunshine duration in a day, hours */ 
+    double FF        /* the fractional forest cover, between 0.0 and 1.0 */ 
 ){
-    /*
-    received longwave radiation on the surface
+    /****
+     * calculate the received downward longwave radiation,
+     *      considering the effect of cloud cover
+     * 
+     * applicable scenarios:
+     * for overstory (canopy): set FF = 0
+     * for understory (ground or snow): FF is between 0 and 1.0
+     * 
     */
     double delta = 4.90 * pow(10, -9);  // Stefan-Boltzmann constant, MJ/m2/k4/d
     double Lin;  // downward longwave radiation considering cloudiness, MJ/m2/d
 
     double emissivity_clr, emissivity_sky, emissivity_at;
+    /**
+     * emissivity_clr: emissivity of clear-sky
+     * emissivity_sky: emissivity considering the effect of cloud cover
+     * emissivity_at:  emissivity of atmosphere
+    */
     double es;  // saturated vapor pressure, kPa;
     double ea;  // actual vapor pressure, kPa;
     es = 0.6108 * exp(17.277 * Tem_air / (Tem_air + 273.3));
@@ -203,7 +226,6 @@ double Radiation_downward_long(
     w_s = acos( - tan(lat * PI / 180) * tan(del));
     N = 24/PI * w_s;
 
-
     emissivity_sky = (1 - n / N) + n / N * emissivity_clr;
     emissivity_at = (1 - FF) * emissivity_sky + FF;
     
@@ -212,6 +234,14 @@ double Radiation_downward_long(
 }
 
 
+/**** references:
+ * Abramowitz G., L. Pouyanné, and H. Ajami (2012). 
+ *      On the information content of surface meteorology 
+ *      for downward atmospheric long-wave radiation synthesis. 
+ *      Geographical Research Letters 39(5), 
+ *      doi: https://doi.org/10.1029/2011GL050726
+ * 
+*/
 
 
 
