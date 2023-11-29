044a28952c30535912a60a83c62207f963d47661
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-11-29 11:17:33 +0100

    update soil desorption header file

--- scr/Soil_Desorption.h
+++ scr/Soil_Desorption.h
@@ -1,6 +1,14 @@
 #ifndef SOIL_DESORPTION
 #define SOIL_DESORPTION
 
+double Soil_Desorption(
+    double Soil_Moisture,
+    double Soil_Conduct_Sat,
+    double Soil_PoreSize_index,
+    double Soil_Porosity,
+    double Soil_Bubbling_pres,
+    int step_time
+);
 
 
 #endif
