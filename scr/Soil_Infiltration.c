4a195ab277442c2f42228026b2449f15de71fd82
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-11-29 11:12:39 +0100

    update soil infiltration code

---
+++ scr/Soil_Infiltration.c
@@ -0,0 +1,17 @@
+
+/*
+ * SUMMARY:      Soil_Percolation.c
+ * USAGE:        Calculate the percolation in unsaturated soil zone
+ * AUTHOR:       Xiaoxiang Guan
+ * ORG:          Section Hydrology, GFZ
+ * E-MAIL:       guan@gfz-potsdam.de
+ * ORIG-DATE:    Nov-2023
+ * DESCRIPTION:  Calculate unsaturated soil zone percolation
+ * DESCRIP-END.
+ * FUNCTIONS:    
+ * 
+ * COMMENTS:
+ * two-layer soil conceptualization: upper layer and lower layer.
+ * 
+ * 
+ */
