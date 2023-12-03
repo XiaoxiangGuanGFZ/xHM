bc9dd56a4d0d8ec723ee509fef0c33aa3734af23
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-12-03 11:41:51 +0100

    update soil infiltration header

--- scr/Soil_Infiltration.h
+++ scr/Soil_Infiltration.h
@@ -1,5 +1,14 @@
 #ifndef SOIL_INFIL
 #define SOIL_INFIL
 
+double Soil_Infiltration(
+    double Water_input,
+    double Soil_Moisture,
+    double Soil_Porosity,
+    double Soil_Conduct_Sat,
+    double Air_Entry_Pres,
+    double b, 
+    int step_time
+);
 
 #endif
