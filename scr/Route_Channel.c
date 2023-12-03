9cf9261c899b2271c48d40cd58488a84a98d570c
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-12-03 14:21:09 +0100

    update channel routing algorithm

--- scr/Route_Channel.c
+++ scr/Route_Channel.c
@@ -15,17 +15,36 @@
  * 
  * 
  * REFERENCES:
- * 
- * 
+ *  
  * 
  */
+
+/******************************************************************
+ * VARIABLES:
+ * double *Qin             - inflow of the river reach, [m3/h]
+ * double *Qout            - outflow of the river reach, [m3/h]
+ * double *Vin             - water volume of river reach at the beginning of the time interval, [m3]
+ * double *Vout            - water volume of river reach after the time interval, [m3]
+ * double k                - storage parameter: equal to the inverse of the average residence time, [1/h]
+ * int step_time           - time step, interval, [h]
+ * 
+ * 
+******************************************************************/
 
 #include <math.h>
 #include "Route_Channel.h"
 
-double Qout_reach(
-
+void Channel_Routing(
+    double *Qin,
+    double *Qout,
+    double *Vin,
+    double *Vout,
+    double k,
+    int step_time
 )
 {
-    
+    *Vout = *Qin / k + 
+        (*Vin - *Qin / k) * exp(- k * (double) step_time);
+    *Qout = *Qin - (*Vout - *Vin) / (double) step_time;
+
 }
--- scr/Route_Channel.h
+++ scr/Route_Channel.h
@@ -1,5 +1,13 @@
 #ifndef ROUTE_CHANNEL
 #define ROUTE_CHANNEL
 
+void Channel_Routing(
+    double *Qin,
+    double *Qout,
+    double *Vin,
+    double *Vout,
+    double k,
+    int step_time
+);
 
 #endif
