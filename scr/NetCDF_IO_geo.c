de1ffe892dabf5b57db326a51ac3d51a17d2575a
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-12-12 19:19:27 +0100

    initialize the nc interface

---
+++ scr/NetCDF_IO_geo.c
@@ -0,0 +1,33 @@
+/*
+ * SUMMARY:      NetCDF_IO_geo.c
+ * USAGE:        
+ * AUTHOR:       Xiaoxiang Guan
+ * ORG:          Section Hydrology, GFZ
+ * E-MAIL:       guan@gfz-potsdam.de
+ * ORIG-DATE:    Dec-2023
+ * DESCRIPTION:  
+ * DESCRIP-END.
+ * FUNCTIONS:    
+ * 
+ * COMMENTS:
+ * 
+ *
+ * REFERENCES:
+ * 
+ *
+ */
+
+/******************************************************************
+ * VARIABLEs:
+ * 
+ * 
+ * 
+ * 
+*******************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netcdf.h>
+
+
+
---
+++ scr/NetCDF_IO_geo.h
@@ -0,0 +1,4 @@
+#ifndef NETCDF_IO_GEO
+#define NETCDF_IO_GEO
+
+#endif
