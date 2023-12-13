979cc949830c5d99018ff70488be7042cc9f280a
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-12-13 12:19:51 +0100

    update 2ASCII function in GEO

---
+++ scr/GEO2ASCII.c
@@ -0,0 +1,24 @@
+/*
+ * SUMMARY:      GEO2ASCII.c
+ * USAGE:        transfer the NetCDF geodata into ASCII text format
+ * AUTHOR:       Xiaoxiang Guan
+ * ORG:          Section Hydrology, GFZ
+ * E-MAIL:       guan@gfz-potsdam.de
+ * ORIG-DATE:    Dec-2023
+ * DESCRIPTION:  
+ * DESCRIP-END.
+ * FUNCTIONS:    
+ * 
+ * COMMENTS:
+ * 
+ *
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "GEO_ST.h"
+#include "GEO2NC.h"
--- scr/GEO2ASCII.h
+++ scr/GEO2ASCII.h
@@ -1,24 +1,7 @@
-/*
- * SUMMARY:      GEO2ASCII.c
- * USAGE:        transfer the NetCDF geodata into ASCII format
- * AUTHOR:       Xiaoxiang Guan
- * ORG:          Section Hydrology, GFZ
- * E-MAIL:       guan@gfz-potsdam.de
- * ORIG-DATE:    Dec-2023
- * DESCRIPTION:  
- * DESCRIP-END.
- * FUNCTIONS:    
- * 
- * COMMENTS:
- * 
- *
- */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <netcdf.h>
-#include <math.h>
-#include <string.h>
-#include "GEO2ASCII.h"
-#include "GEO_ST.h"
+#ifndef GEO2ASCII
+#define GEO2ASCII
 
+
+
+#endif
