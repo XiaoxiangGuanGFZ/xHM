b947360fb5891df33103849655274a046e7220b4
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-12-28 10:37:06 +0100

    rename

--- scr/NetCDF_IO_geo.h
+++ scr/NetCDF_IO_geo.h
@@ -1,4 +1,18 @@
 #ifndef NETCDF_IO_GEO
 #define NETCDF_IO_GEO
 
+#include <time.h>
+
+void Write2NC(
+    char varNAME[],
+    char att_unit[],
+    char att_longname[],
+    char FP_GEO[],
+    char FP_output[],
+    double **rdata,
+    time_t start_time,
+    int step_time,
+    int ts_length
+);
+
 #endif
