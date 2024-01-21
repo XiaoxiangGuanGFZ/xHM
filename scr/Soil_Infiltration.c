1bfa30fe6b601db4c63c1b40f5859f7a50f9ef17
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2024-01-21 04:05:48 +0100

    scenario: water input close to sat_hydro_con

--- scr/Soil_Infiltration.c
+++ scr/Soil_Infiltration.c
@@ -37,6 +37,7 @@
  * 
 ****************************************************************/
 
+#include <stdio.h>
 #include <math.h>
 #include "Soil_Infiltration.h"
 
@@ -59,7 +60,7 @@ double Soil_Infiltration(
          * the water input rate is less than the 
          * saturated hydraulic conductivity
         */
-        Infiltration = Water_input * (double) step_time;
+        Infiltration = Water_input * step_time;
     }
     else
     {
@@ -87,21 +88,64 @@ double Soil_Infiltration(
                     1 + Water_input * Tp / Tension_effective / (Soil_Porosity - Soil_Moisture)
                 );
         Te = step_time - Tp + Tc;
-        Infiltration = Soil_Conduct_Sat * (
-            0.529 * Te + 
-            0.471 * pow(
-                Tx * Te + Te * Te, 0.5
-            ) + 
-            0.138 * Tx * (
-                log(Te + Tx) - log(Tx)
-            ) + 
-            0.471 * Tx * (
-                log(
-                    Te + Tx/2 + pow(Tx * Te + Te * Te, 0.5)
-                ) - log(Tx / 2)
-            )
-        );
+        if (Te < 0.0)
+        {
+            // if (Water_input <= Soil_Conduct_Sat + 0.00082)
+            // {
+            //     Infiltration = Soil_Conduct_Sat * ((double)step_time);
+            // }
+            /***************************
+             * we found that for the clay soil type, the ponding time could be
+             * extraordinarily long, longer than step_time + Tc, when the water input is
+             * very close to the saturated hydraulic
+             * conductivity (of clay), therefore we increased infiltration threshold.
+             */
+            Infiltration = Soil_Conduct_Sat * step_time;
+        }
+        else
+        {
+            Infiltration = Soil_Conduct_Sat * (0.529 * Te +
+                                               0.471 * pow(
+                                                           Tx * Te + Te * Te, 0.5) +
+                                               0.138 * Tx * (log(Te + Tx) - log(Tx)) +
+                                               0.471 * Tx * (log(Te + Tx / 2 + pow(Tx * Te + Te * Te, 0.5)) - log(Tx / 2)));
+            // printf("Infiltration: %f\n", Infiltration);
+            if ((int)(Infiltration * 1000) <= 0.0)
+            {
+                Infiltration = Soil_Conduct_Sat * step_time;
+            }
+        }
+
+
     }
-    
     return Infiltration;
 }
+
+// int main(int argc, char const *argv[])
+// {
+//     double Water_input[18] = {0.004, 0.0094, 0.01, 0.015, 0.018, 
+//     0.02, 0.022, 0.024, 0.026, 0.028, 
+//     0.03, 0.035, 0.038, 0.04, 0.042, 
+//     0.044, 0.05,0.06}; // m
+//     double Soil_Moisture = 0.2;
+//     double Soil_Porosity = 0.482;
+//     double Soil_Conduct_Sat = 0.00078;
+//     double Air_Entry_Pres = 0.405;
+//     double b = 11.4;
+//     int step_time = 24;
+//     int i;
+//     double SI;
+//     for (i = 0; i < 18; i++)
+//     {
+//         SI = Soil_Infiltration(
+//         Water_input[i] / step_time,
+//         Soil_Moisture,
+//         Soil_Porosity,
+//         Soil_Conduct_Sat,
+//         Air_Entry_Pres,
+//         b,
+//         step_time);
+//         printf("%f %f\n", Water_input[i] * 1000, SI * 1000);
+//     }
+//     return 0;
+// }
