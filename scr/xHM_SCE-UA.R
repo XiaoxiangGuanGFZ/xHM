1a944653640e8d0d6695e8aa5203c7d6f3a1748d
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2024-01-24 14:29:58 +0100

    create sceua-xhm

--- scr/xHM_SCE-UA.R
+++ scr/xHM_SCE-UA.R
@@ -11,172 +11,79 @@
 library(rtop)
 library(tidyverse)
 
-WBM = function(A,P,Er,Discharge,Tem,Ks,Kg,Ksn,Smax){
-  # A:流域面积（km2）
-  # P：流域面均月降水量（mm）
-  # Er：流域面均月潜在蒸散发量(mm)
-  # Discharge：出口断面月均流量（m3/s）
-  # Tem：流域面均月气温（℃）
-  # Ks：地表径流出流系数，
-  # Kg：地下径流出流系数，
-  # Ksn：融雪径流系数，
-  # Smax：土壤蓄水容量（mm）
-  S0 = 1
-  SN0 = 0.2*Smax
-  n = length(P)   #length of data sequence
-  Pr = NULL   #rainfall /mm
-  Psn = NULL  #snow-melt /mm
-  Qs = NULL   #surface flow
-  Qg = NULL   #groundwater discharge
-  Qsn = NULL  #snow-melt driven runoff
-  Q = NULL    #total runoff
-  S = NULL    #soil moisture
-  SN = NULL   #snow accumulation
-  E = NULL    #actual evaporation
-  
-  for (i in 1:n) {
-    if (i == 1) {
-      S_p = S0   #S_p antecedent soil moisture
-      SN_p = SN0  #SN_p antecedent snow accumulation
-      #Qsn_p = 0.5
-    }
-    else {
-      S_p = S[i-1]
-      SN_p = SN[i-1]
-      #Qsn_p = Qsn[i-1]
-    }
-    
-    if (Tem[i] <= -3) {
-      Pr[i] = 0
-      Psn[i] = P[i]
-      Qs[i] = 0
-      Qsn[i] = 0
-      Qg[i] = Kg*S_p
-      
-      E[i] = 0.2*Psn[i] #20% precipitated snow for Snow evaporation
-      SN[i] = SN_p + 0.8*Psn[i] #80% snow accumulated
-      S[i] = S_p - Qg[i]
-      #*****soil moisture correction*****
-      if (S[i] <= 0.08*Smax) {
-        Qg[i] = Qg[i] + (S_p - 0.08*Smax)*Qg[i]/(Qg[i]+Er[i]+0.001)
-        E[i] = E[i] + (S_p - 0.08*Smax)*(Er[i]+0.001)/(Qg[i]+Er[i]+0.001)
-        S[i] = 0.08*Smax
-      }
-      if (S[i] >= Smax) {
-        Qg[i] = Qg[i] + S_p - Smax
-        S[i] = Smax
-      }
-      
-    } else if (Tem[i] > -3 & Tem[i] <= 3) {
-      Pr[i] = P[i]*(Tem[i]+3)/6
-      Psn[i] = P[i]*(3-Tem[i])/6
-      SN[i] = SN_p+Psn[i]
-      Qsn[i] = Ksn*SN[i]*exp((Tem[i]-3)/6)
-      SN[i] = SN[i] - Qsn[i]
-      
-      #*******snow storage correction****
-      if (SN[i] < 0 ) {
-        Qsn[i] = SN_p+Psn[i]
-        SN[i] = 0
-      }
-      Qg[i] = Kg*S_p
-      Qs[i] = Ks*Pr[i]*S_p/Smax
-      
-      S[i] = S_p-Qg[i]+Pr[i]-Qs[i]
-      E[i] = Er[i]*S[i]/Smax
-      S[i] = S[i]-E[i]
-      total = Qs[i] + Qg[i] + Er[i]+0.01
-      #*****soil moisture correction*****
-      if (S[i] > Smax) {
-        Qs[i] = Qs[i] + (S[i] - Smax)*Qs[i]/total
-        Qg[i] = Qg[i] + (S[i] - Smax)*Qg[i]/total
-        E[i] = E[i] + (S[i] - Smax)*(E[i]+0.01)/total
-        S[i] = Smax
-      }
-      if (S[i] <= 0.08*Smax) {
-        Qs[i] = Qs[i] + (S[i] - 0.08*Smax)*Qs[i]/total
-        Qg[i] = Qg[i] + (S[i] - 0.08*Smax)*Qg[i]/total
-        E[i] = E[i] + (S[i] - 0.08*Smax)*(E[i]+0.01)/total
-        S[i] = 0.08*Smax
-      }
-      
-    } else { #Tem[i] > 3
-      Pr[i] = P[i]
-      Psn[i] = 0
-      if (SN_p > 0) {
-        Qsn[i] = 0.5*Ksn*SN_p
-        Pr[i] = Pr[i] + SN_p - Qsn[i]
-        SN[i] = 0
-        
-      } else {
-        Qsn[i] = 0
-        SN[i] = 0
-        Pr[i] = Pr[i] + SN_p
-      }
-      Qs[i] = Ks*Pr[i]*S_p/Smax
-      Qg[i] = Kg*S_p
-      E[i] = Er[i]*S_p/Smax
-      S[i] = S_p+Pr[i]-Qs[i]-Qg[i]-E[i]
-      total = Qs[i] + Qg[i] +Er[i]+0.01
-      #*****soil moisture correction*****
-      if (S[i] > Smax) {
-        Qs[i] = Qs[i] + (S[i] - Smax)*Qs[i]/total
-        Qg[i] = Qg[i] + (S[i] - Smax)*Qg[i]/total
-        E[i] = E[i] + (S[i] - Smax)*(E[i]+0.01)/total
-        S[i] = Smax
-      }
-      if (S[i] <= 0.08*Smax) {
-        Qs[i] = Qs[i] + (S[i] - 0.08*Smax)*Qs[i]/total
-        Qg[i] = Qg[i] + (S[i] - 0.08*Smax)*Qg[i]/total
-        E[i] = E[i] + (S[i] - 0.08*Smax)*(E[i]+0.01)/total
-        S[i] = 0.08*Smax
-      }
-    }
-    
-    Q[i] = Qs[i] + Qg[i] + Qsn[i]  #adjusted total runoff
-  }
-  R_si = Q   #unit:mm
-  R_re = Discharge*30*24*3.6/A #unit:mm
-  out = data.frame(R_si,R_re)
-  colnames(out) = c('R_si','R_re')
-  return(out)
-}
+fp_xHM <<- "D:/xHM/scr/xHM.exe "
+fp_gp_tem <<- "D:/xHM/example_data/Global_Para_tem.txt"
+fp_gp <<- "D:/xHM/example_data/Global_Para_SCEUA.txt"
+df_gp <<- data.frame(
+  field = c("SOIL_D", "SOIL_d1", "SOIL_d2", "ROUTE_CHANNEL_k"),
+  value = c(1.5, 0.2, 0.2, 2)
+)
+
+Qobs <<- read.table(
+  "D:/xHM/example_data/Qobs/Qobs_daily_Jianning.txt",
+  header = T
+) %>% mutate(
+  date = as.Date(time)
+) %>% filter(
+  date >= as.Date("2004-01-01"),
+  date <= as.Date("2008-12-31")
+)
+
+
+data_start <<- as.Date("2003-10-01")
+data_end <<- as.Date("2013-12-31")
+dt_ts <<- seq(data_start,
+              data_end,
+              1)
+
 #-------------拟合度函数----------
-fit_WBM = function(x) {
-  Ks = x[1];Kg = x[2];Ksn = x[3];Smax = x[4]
-  out = WBM(Validinput_DAREA,Validinput_P,Validinput_E,Validinput_Q,Validinput_T,
-            Ks,Kg,Ksn,Smax)
-  Qsi = out$R_si
-  Qre = out$R_re
+fit_xHM = function(x) {
+  file.copy(fp_gp_tem, fp_gp, overwrite = TRUE)
+  df_gp$value = x
+  write.table(
+    df_gp,
+    file = fp_gp,
+    col.names = F, row.names = F, quote = F, append = T, sep = ','
+  )
+  system(paste0(fp_xHM, fp_gp))
+  
+  Qsim = read.table(
+    "D:/xHM/output/Qout_outlet0.txt",
+    header = F, skip = 10
+  ) %>% mutate(
+    date = dt_ts
+  ) %>% filter(
+    date >= as.Date("2004-01-01"),
+    date <= as.Date("2008-12-31")
+  )
+  Qre = Qobs$Qobs
+  Qsi = Qsim[,4]
   R2 = 1-sum((Qre-Qsi)^2,na.rm = T)/sum((Qre-mean(Qre))^2,na.rm = T) #NSE
   return(1-R2)
 }
-#----------------SCEUA-WBM-Main----------------
+#----------------SCEUA-xHM-Main----------------
 
+paralower = c(1  , 0.05,  0.05, 0.01)
+paraupper = c(3  , 0.5, 0.5, 5)
+parainitial = c(1.5, 0.2, 0.2, 0.4)
 
-dat = read.table("J:/王国庆老师/小论文3-SWBM模型区域适用性研究/6个典型站点/石角－38363/data.csv",header = F,sep = ",")
-dat = dat[dat[,1]<=1970,]
-Validinput_P <<- dat[,3]
-Validinput_E <<- dat[,6]
-Validinput_Q <<- dat[,5]
-Validinput_T <<- dat[,4]
-Validinput_DAREA <<- 38363
+sceua_xHM = sceua(fit_xHM, pars = parainitial, lower = paralower, 
+                  upper = paraupper, maxn = 2000, pcento = 0.0001,iprint = 0)
+fit(sceua_xHM$par)
+para = sceua_xHM$par
 
-paralower = c(0  ,0 ,  50, 0)
-paraupper = c(1  ,1 , 450, 0.1)
-parainitial=c(0.2,0.2 ,200,0.002)
-
-sceua_WBM = sceua(fit_WBM, pars = parainitial, lower = paralower, 
-                  upper = paraupper, maxn = 10000, pcento = 0.00001,iprint = 0)
-fit(sceua_WBM$par)
-para = sceua_WBM$par
-Ks = para[1];Kg = para[2];Ksn = para[3];Smax = para[4]
-out = WBM(Validinput_DAREA,Validinput_P,Validinput_E,Validinput_Q,Validinput_T,
-          Ks,Kg,Ksn,Smax)
-Qsi = out$R_si
-Qre = out$R_re
-ma = max(c(Qsi,Qre))
-plot(Qre,Qsi,type = "p",xlim=c(0,ma),ylim = c(0,ma))
-
-
+Qsim = read.table(
+  "D:/xHM/output/Qout_outlet0.txt",
+  header = F, skip = 10
+) %>% mutate(
+  date = dt_ts
+) %>% filter(
+  date >= as.Date("2004-01-01"),
+  date <= as.Date("2008-12-31")
+)
+Qre = Qobs$Qobs
+Qsi = Qsim[,4]
+R2 = 1-sum((Qre-Qsi)^2,na.rm = T)/sum((Qre-mean(Qre))^2,na.rm = T) #NSE
+plot(Qobs$date, Qobs$Qobs, type = "p", col = "black")
+points(Qsim$date, Qsim[,1] + Qsim[,3], type = "l", col = "red")
 
